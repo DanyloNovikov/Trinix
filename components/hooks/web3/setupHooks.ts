993db3150ad1238452ef645463a6a872f33d5e35
Danylo Novikov <danylo.novikov@syndicode.com>
2022-06-16 19:55:32 +0300

    Add hook to listed Nfts

--- components/hooks/web3/setupHooks.ts
+++ components/hooks/web3/setupHooks.ts
@@ -1,11 +1,13 @@
 import { Web3Dependencies } from "@_types/hooks";
 import { hookFactory as createAccountHook, UseAccountHook } from "@hooks/web3/useAccount";
 import { hookFactory as createNetworkHook, UseNetworkHook } from "@hooks/web3/useNetwork";
+import { hookFactory as createListedNftsHook, UseListedNftsHook } from "@hooks/web3/useListedNfts";
 
 // set type returned for our function setting hooks
 export type Web3Hooks = {
     useAccount: UseAccountHook;
     useNetwork: UseNetworkHook;
+    useListedNfts: UseListedNftsHook;
 }
 
 // set type params for function
@@ -16,8 +18,8 @@ export type SetupHooks = {
 // create function for setting our hooks
 export const setupHooks: SetupHooks = (deps) => {
     return {
-        // c
         useAccount: createAccountHook(deps),
-        useNetwork: createNetworkHook(deps)
+        useNetwork: createNetworkHook(deps),
+        useListedNfts: createListedNftsHook(deps)
     }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,12 @@ import { NftList } from '@ui';
 import nfts from '../content/meta.json';
 import { NftMeta } from '@_types/nft';
 import { useWeb3 } from '@providers/web3';
+import { useListedNfts } from '@hooks/web3';
 
 const Home: NextPage = () => {
-    const { provider, contract } = useWeb3();
+    const { nfts } = useListedNfts();
 
-    const getAccounts = async () => {
-        const account = await provider!.listAccounts();
-        console.log(account[0])
-    }
-
-    if (provider) {
-        provider.listAccounts();
-    }
-
-
+    console.log(nfts.data);
     return (
         <div className="relative pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
 
@@ -28,12 +20,10 @@ const Home: NextPage = () => {
                     <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Amazing Creatures
                         NFTs</h2>
                     <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-                        { `isLoading:  ${provider}` }
-
                         Mint a NFT to get unlimited ownership forever!
                     </p>
                 </div>
-                <NftList nfts={nfts as NftMeta[]}/>
+                <NftList nfts={nfts.data}/>
             </div>
         </div>
     )
