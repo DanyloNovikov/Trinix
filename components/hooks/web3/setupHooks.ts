c392a8beefe92eba746f3fcd0ea6a03323b16718
Danylo Novikov <danylo.novikov@syndicode.com>
2022-06-17 20:27:29 +0300

    Display a NFT into account/profile | display price

--- components/hooks/web3/setupHooks.ts
+++ components/hooks/web3/setupHooks.ts
@@ -2,12 +2,14 @@ import { Web3Dependencies } from "@_types/hooks";
 import { hookFactory as createAccountHook, UseAccountHook } from "@hooks/web3/useAccount";
 import { hookFactory as createNetworkHook, UseNetworkHook } from "@hooks/web3/useNetwork";
 import { hookFactory as createListedNftsHook, UseListedNftsHook } from "@hooks/web3/useListedNfts";
+import { hookFactory as createOwnedNftsHook, UseOwnedNftsHook } from "@hooks/web3/useOwnedNfts";
 
 // set type returned for our function setting hooks
 export type Web3Hooks = {
     useAccount: UseAccountHook;
     useNetwork: UseNetworkHook;
     useListedNfts: UseListedNftsHook;
+    useOwnedNfts: UseOwnedNftsHook;
 }
 
 // set type params for function
@@ -20,6 +22,7 @@ export const setupHooks: SetupHooks = (deps) => {
     return {
         useAccount: createAccountHook(deps),
         useNetwork: createNetworkHook(deps),
-        useListedNfts: createListedNftsHook(deps)
+        useListedNfts: createListedNftsHook(deps),
+        useOwnedNfts: createOwnedNftsHook(deps)
     }
 }
