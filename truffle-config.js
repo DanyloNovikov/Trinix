30c7b3149b1c107ba11a2dcd6152169c827a413a
Danylo Novikov <danylo.novikov@syndicode.com>
2022-06-19 20:58:06 +0300

    Setup up before deploy to production

--- truffle-config.js
+++ truffle-config.js
@@ -1,31 +1,30 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const keys = require("./keys.json");
+console.log(keys.PRIVATE_KEY)
 module.exports = {
-    contracts_build_directory: "./public/contracts",
-    networks: {
-        development: {
-            host: "127.0.0.1",
-            // change for your ganache port
-            port: 7545,
-            network_id: "*",
-        },
+  contracts_build_directory: "./public/contracts",
+  networks: {
+    development: {
+     host: "127.0.0.1",
+     port: 7545,
+     network_id: "*",
     },
-
-    // Set default mocha options here, use special reporters, etc.
-    mocha: {
-        // timeout: 100000
-    },
-
-    // Configure your compilers
-    compilers: {
-        solc: {
-            version: "0.8.14",      // Fetch exact version from solc-bin (default: truffle's version)
-            // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-            // settings: {          // See the solidity docs for advice about optimization and evmVersion
-            //  optimizer: {
-            //    enabled: false,
-            //    runs: 200
-            //  },
-            //  evmVersion: "byzantium"
-            // }
-        }
-    },
+    ropsten: {
+      provider: function () {        
+        return new HDWalletProvider(
+            keys.PRIVATE_KEY,
+            keys.INFURA_ROPSTEN_URL
+      )},
+      network_id: '3',
+      gas: 5500000,
+      gasPrice: 20000000000,
+      confirmations: 2,
+      timeoutBlocks: 200
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.13",
+    }
+  },
 };
